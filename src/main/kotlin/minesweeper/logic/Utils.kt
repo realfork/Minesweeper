80877302d4912d54b97dbd2ace21a62142181493
fork <70575926+realfork@users.noreply.github.com>
2022-11-21 09:52:04 +0000

    Added nicer visuals, made colors accurate to Google Minesweeper

--- src/main/kotlin/minesweeper/logic/Utils.kt
+++ src/main/kotlin/minesweeper/logic/Utils.kt
@@ -5,11 +5,11 @@ import java.awt.Color
 object Utils {
     fun numberToColor(number: Int): Color {
         return when (number) {
-            1 -> Color.BLUE
-            2 -> Color.GREEN
-            3 -> Color.RED
-            4 -> Color.MAGENTA
-            5 -> Color.YELLOW
+            1 -> Color(28, 119, 208)
+            2 -> Color(56, 142, 60)
+            3 -> Color(211, 47, 47)
+            4 -> Color(123, 31, 162)
+            5 -> Color(255, 143, 0)
             6 -> Color.CYAN
             7 -> Color.DARK_GRAY
             8 -> Color.PINK
