e47dcc0336fec2984ba217557988a06bc0af129b
fork <brennandodds@gmail.com>
2023-02-03 13:27:09 +0000

    Cleaned up code.

--- src/main/kotlin/minesweeper/logic/Tile.kt
+++ src/main/kotlin/minesweeper/logic/Tile.kt
@@ -1,16 +1,21 @@
 package minesweeper.logic
 
-class Tile(val x: Int, val y: Int) {
+import java.awt.Color
+import java.awt.Dimension
+import javax.swing.JButton
+
+class Tile(private val x: Int, private val y: Int) {
+    // LOGIC
     var isBomb = false
     private var number = 0
 
     fun getNumber() = number
     fun increment() = number++
 
-    fun up(): Tile? = Board.tiles.find { it.x == this.x && it.y == this.y + 1 }
-    fun down(): Tile? = Board.tiles.find { it.x == this.x && it.y == this.y - 1 }
-    fun left(): Tile? = Board.tiles.find { it.x == this.x - 1 && it.y == this.y }
-    fun right(): Tile? = Board.tiles.find { it.x == this.x + 1 && it.y == this.y }
+    private fun up(): Tile? = Board.tiles.find { it.x == this.x && it.y == this.y + 1 }
+    private fun down(): Tile? = Board.tiles.find { it.x == this.x && it.y == this.y - 1 }
+    private fun left(): Tile? = Board.tiles.find { it.x == this.x - 1 && it.y == this.y }
+    private fun right(): Tile? = Board.tiles.find { it.x == this.x + 1 && it.y == this.y }
 
     fun surrounding(): List<Tile> {
         return listOfNotNull(
@@ -23,5 +28,37 @@ class Tile(val x: Int, val y: Int) {
             down(),
             down()?.right()
         )
+    }
+
+    // UI
+    val button = JButton("")
+
+    // Colors
+    private var grassColor = if (x % 2 == y % 2) Color(170, 215, 81) else Color(162, 209, 73)
+    private var tileColor = if (x % 2 == y % 2) Color(229, 194, 159) else Color(215, 184, 153)
+
+    // Flag logic
+    private var isFlagged = false
+    fun isFlagged() = isFlagged
+    fun updateFlag() {
+        isFlagged = !isFlagged
+        button.text = if (isFlagged) "X" else ""
+        button.foreground = if (!isFlagged) Utils.numberToColor(number) else Color.RED
+    }
+
+    // Reveal logic
+    private var isRevealed = false
+    fun isRevealed() = isRevealed
+    fun reveal() {
+        button.background = tileColor
+        if (number > 0) {
+            button.foreground = Utils.numberToColor(number)
+            button.text = number.toString()
+        }
+    }
+
+    init {
+        button.background = grassColor
+        button.preferredSize = Dimension(50, 50)
     }
 }
